a8fd17fd350cf7f97f6e69e1986bc6edd1e9619a
Bard Torustad <bard.torustad@forskningsradet.no>
2020-07-20 23:29:37 +0200

    initial commit

---
+++ medium-customer/src/test/java/no/torustad/medium/mediumcustomer/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package no.torustad.medium.mediumcustomer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
